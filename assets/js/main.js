d89da7e6ec4e24d019981c8338886f33b85af355
Toma <lxsunbin@qq.com>
2017-09-19 22:52:37 +0800

    enter

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,11 @@ var App=angular.module("App",[
 
 var assetsPath="./assets/";
 
-App.controller("AppController",function($rootScope,$scope,$timeout,$state){   
+App.controller("AppController",function($rootScope,$scope,$timeout,$state,$location){  
+    //清除页面缓存
+    $scope.$on('$locationChangeStart',function (event, newUrl, oldUrl) {
+        $location.search('_v', new Date().getTime())
+    });
    //顶部轮播
    $scope.swiperArr=[];
    for(var i=1;i<11;i++){
@@ -54,27 +58,30 @@ App.controller("AppController",function($rootScope,$scope,$timeout,$state){
    //左侧导航
    $scope.changeMenu=function(event){
         $('.menu_list').slideUp();
-        $('.menu_arrow').css('transform','rotate(0deg)');
+        $('.menu_arrow').removeClass('current');
         $('.nav_link').removeClass('current');
         if($(event.target).parents('li').find('.menu_list').is(':hidden')){
             $(event.target).parents('li').find('.menu_list').slideDown();
-            $(event.target).parents('li').find('.menu_arrow').css('transform','rotate(90deg)');
+            $(event.target).parents('li').find('.menu_arrow').addClass('current');
             $(event.target).parents('li').find('.nav_link').addClass('current');
         }else{
             $(event.target).parents('li').find('.menu_list').slideUp();
-            $(event.target).parents('li').find('.menu_arrow').css('transform','rotate(0deg)');
+            $(event.target).parents('li').find('.menu_arrow').removeClass('current');
             $(event.target).parents('li').find('.nav_link').removeClass('current');
         }
     };
-    $scope.listActive=function(event){
-        $('.menu_list li a').removeClass('current');
-        $(event.target).addClass('current');
-    }
 });
-App.run(["$rootScope", "$state", function ($rootScope, $state) {
-    $rootScope.$state = $state; // state to be accessed from view
-    // $rootScope.$on("$stateChangeSuccess",  function(event, toState, toParams, fromState, fromParams) {  
-    //     console.log('changed')
-    // });  
+//全局参数及路由跳转设置
+App.run(["$rootScope", "$state","$location", function ($rootScope, $state,$location) {
+    $rootScope.$state=$state; // state to be accessed from view   
+    $rootScope.$on('$stateChangeStart',function (event,next,current) {
+        $rootScope.currentStatus=next.name;
+        if($('.menu_list li a.current').parents('.menu_list').is(':hidden')){
+            $('.menu_list').slideUp();
+            $('.menu_arrow').removeClass('current');
+        };     
+        $('.menu_list li a.current').parents('.menu_list').slideDown(); 
+        $('.menu_list li a.current').parents('li').find('.menu_arrow').addClass('current');    
+    });
 }]);
 
--- assets/js/states/LiveVideo/index.js
+++ assets/js/states/LiveVideo/index.js
@@ -2,14 +2,14 @@ App.config(function($stateProvider,$urlRouterProvider,$locationProvider){
     // $urlRouterProvider.otherwise('error.html');
     //1号直播间
     $stateProvider.state('live1', {
-        url: "LiveVideo/live1.html",
+        url: "/LiveVideo/live1.html",
         templateUrl: 'views/LiveVideo/live1.html',
         data: {pageTitle: '24H猫咪直播间---1号直播间'},
         controller:'live1Controller',
         resolve:{
             deps: ['$ocLazyLoad', function ($ocLazyLoad) {
                 return $ocLazyLoad.load({
-                    name: 'live1',
+                    name: 'App',
                     files: ['js/controller/LiveVideo/live1Controller.js']
                 })
             }]
@@ -17,14 +17,14 @@ App.config(function($stateProvider,$urlRouterProvider,$locationProvider){
     }) 
     //2号直播间
     .state('live2', {
-        url: "LiveVideo/live2.html",
+        url: "/LiveVideo/live2.html",
         templateUrl: 'views/LiveVideo/live2.html',
         data: {pageTitle: '随缘直播间（游戏，户外）---2号直播间'},
         controller:'live2Controller',
         resolve:{
             deps: ['$ocLazyLoad', function ($ocLazyLoad) {
                 return $ocLazyLoad.load({
-                    name: 'live2',
+                    name: 'App',
                     files: ['js/controller/LiveVideo/live2Controller.js']
                 })
             }]
